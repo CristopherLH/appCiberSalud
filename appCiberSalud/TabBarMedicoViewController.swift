2eeb2a28030de40d3365169a489bf42173f89b77
Jonathan Castillo <jonathancastillo@MacBook-de-Jonathan-Castillo-2.local>
2020-12-10 11:23:37 +0100

    v1

---
+++ appCiberSalud/TabBarMedicoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TabBarMedicoViewController.swift
+//  appCiberSalud
+//
+//  Created by Jonathan Castillo on 10/12/20.
+//  Copyright © 2020 Jonathan Castillo. All rights reserved.
+//
+
+import UIKit
+
+class TabBarMedicoViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- appCiberSalud/ViewController.swift
+++ appCiberSalud/ViewController.swift
@@ -9,12 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBAction func btnRegister(_ sender: Any) {
+        performSegue(withIdentifier: "goRegister", sender: nil)
+    }
 
-
+    @IBAction func btnLogin(_ sender: Any) {
+        performSegue(withIdentifier: "goTabBarMain", sender: nil)
+    }
 }
 
